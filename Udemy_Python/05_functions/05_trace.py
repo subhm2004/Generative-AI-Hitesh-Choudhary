a443f9cf51126bd81883d647fd9e55e318400db5
subhm2004 <subhu04012003@gmail.com>
2026-04-21 17:28:45 +0530

    Refactor print statements for clarity and consistency in scope examples

--- Udemy_Python/05_functions/05_trace.py
+++ Udemy_Python/05_functions/05_trace.py
@@ -3,7 +3,10 @@ def add_vat(price, vat_rate):
 
 
 orders = [100, 150, 200]
-
+total_vat = 0
 for price in orders:
     final_amount = add_vat(price, 10)
+    total_vat += final_amount - price
     print(f"Original: {price}, Final with VAT: {final_amount}")
+
+print(f"Total VAT: {total_vat}")
--- Udemy_Python/05_functions/06_scopes.py
+++ Udemy_Python/05_functions/06_scopes.py
@@ -1,20 +1,21 @@
 def serve_chai():
     chai_type = "Masala" # local scope
-    print(f"Inside function {chai_type}")
+    print(f"Inside function: {chai_type}")
 
 
 chai_type = "Lemon"
 serve_chai()
 print(f"Outside function: {chai_type}")
 
+print("\n")
 
 def chai_counter():
     chai_order = "lemon" # Enclosing scope
     def print_order():
         chai_order = "Ginger"
-        print("Inner:", chai_order)
+        print("Inner_loop:", chai_order)
     print_order()
-    print("Outer: ", chai_order)
+    print("Outer_loop: ", chai_order)
 
 chai_order = "Tulsi" # Global
 chai_counter()
