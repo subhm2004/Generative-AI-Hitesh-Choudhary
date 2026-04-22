b04581ee99754810b76e4e931953f8bdbbd97bd8
subhm2004 <subhu04012003@gmail.com>
2026-04-22 16:00:13 +0530

    updated code

--- Udemy_Python/06_chai_business/main.py
+++ Udemy_Python/06_chai_business/main.py
@@ -1,9 +1,9 @@
-# import recipes.flavors
+import recipes.flavors
 
-# print(recipes.flavors.ginger_chai())
+print(recipes.flavors.ginger_chai())
 
 
-# from recipes.flavors import elachai_chai, ginger_chai
+from recipes.flavors import elachai_chai, ginger_chai
 
-# print(ginger_chai())
+print(ginger_chai())
 
