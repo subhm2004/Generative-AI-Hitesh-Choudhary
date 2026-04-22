7f6e9336cf06608872321d07724b307169046ad2
subhm2004 <subhu04012003@gmail.com>
2026-04-22 16:02:29 +0530

    Add additional print statements for elachai_chai and refactor import statements for clarity

--- Udemy_Python/06_chai_business/main.py
+++ Udemy_Python/06_chai_business/main.py
@@ -1,8 +1,16 @@
+# ek ye method h 
 import recipes.flavors
 
 print(recipes.flavors.ginger_chai())
+print(recipes.flavors.elachai_chai())
 
+# ek ye hai
+from recipes import flavors
 
+print(flavors.ginger_chai())
+print(flavors.elachai_chai())
+
+# or ek ye hai 
 from recipes.flavors import elachai_chai, ginger_chai
 
 print(ginger_chai())
